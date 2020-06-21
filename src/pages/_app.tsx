87d77e9df3f7a655cb1bbcb136f540d56314880f
codemesid <siddhanthbajoria@gmail.com>
2020-06-21 18:52:32 +0530

    next added

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const AppComponent = ({ Component, pageProps }) => {
+    return (
+        <>
+            <Component {...pageProps} />
+        </>
+    );
+}
+
+AppComponent.getInitialProps = async (appContext) => {
+    let pageProps = {};
+
+    if (appContext.Component.getInitialProps) {
+        pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    }
+
+    return {
+        pageProps
+    }
+}
+
+export default AppComponent;
