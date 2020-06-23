eed3e5bfaf3a148a6058d94454d9e92b3fe0dc74
codemesid <siddhanthbajoria@gmail.com>
2020-06-23 10:51:22 +0530

    errors and middle wares added

--- server/index.ts
+++ server/index.ts
@@ -3,12 +3,15 @@ import { Request, Response } from "express";
 import { router } from "./routes";
 
 import { app, handler, nextApp } from "./app";
+import { errorHandler } from "./middlewares/error-handler";
 
 const start = async () => {
   try {
     await nextApp.prepare();
     // middleware for server routes -> /api/<name or the route>
     app.use("/api", router);
+    // handles all the error thrown in api routes
+    app.use(errorHandler);
     // middleware for client routes -> /<name or the route>
     app.all("*", (req: Request, res: Response) => handler(req, res));
     // server start listening here
