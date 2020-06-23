d804e98008376383023986ec3814a23f7ffe4a51
codemesid <siddhanthbajoria@gmail.com>
2020-06-23 16:36:14 +0530

    current user added on getInitalProps

--- src/service/auth-route.ts
+++ src/service/auth-route.ts
@@ -1,11 +1,20 @@
 export const checkAuthRoutes = (ctx: any, currentUser: any) => {
   // add routes here that you want to restrict for unauthenticated users
   const authRoutes: string[] = []; // example "/dummy"
-  //   const unAuthRoutes: string[] = [];
+  // add routes here that you want to restrict for authenticated users
+  const unAuthRoutes: string[] = []; // example "/login"
   const { res, pathname } = ctx;
   if (res) {
-    console.log(authRoutes.indexOf(pathname));
-    if (authRoutes.indexOf(pathname) >= 0) {
+    // if someone tries to enter protected route and is not authenticated
+    if (authRoutes.indexOf(pathname) >= 0 && !!!currentUser) {
+      res.writeHead(302, "not allowed", {
+        // location to be changed to /login , once page is setup
+        location: "/",
+      });
+      res.end();
+    }
+    // if someone tries to enter unprotected route and is already authenticated
+    if (unAuthRoutes.indexOf(pathname) >= 0 && !!currentUser) {
       res.writeHead(302, "not allowed", {
         location: "/",
       });
