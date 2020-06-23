cc63b24465e660b8a9543231b3fa574618cd2d3f
codemesid <siddhanthbajoria@gmail.com>
2020-06-23 16:05:21 +0530

    validation to be completed template added

---
+++ server/validator/user-validator.ts
@@ -0,0 +1,9 @@
+import { body } from "express-validator";
+
+// TODO add validations for signup
+// lookup the controller for the following and write validator for the field you get form req.body
+// export const signupValidator = [];
+
+// TODO add validations for signin
+// lookup the controller for the following and write validator for the field you get form req.body
+// export const signinValidator = [];
