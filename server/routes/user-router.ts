5debebd7bc09c67f44e6910c36476d9d0e515bb0
codemesid <siddhanthbajoria@gmail.com>
2020-06-23 16:04:53 +0530

    user router added

---
+++ server/routes/user-router.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response } from "express";
+// TODO once you write validator uncomment the below
+// import { signupValidator, signinValidator } from "../validator/user-validator";
+import {
+  signupController,
+  signinController,
+  currentUserController,
+} from "../controller/user-controller";
+
+import { validateRequest } from "../middlewares/validate-request";
+import { currentUser } from "../middlewares/current-user";
+
+const router = express.Router();
+
+// to fetch current user cookie info
+router.get("/currentUser", currentUser, currentUserController);
+
+// TODO once you write validators ,add the validator just before validateRequest
+// to add new User
+router.post("/signup", validateRequest, signupController);
+
+// TODO once you write validators ,add the validator just before validateRequest
+// to login new User
+router.post("/signin", validateRequest, signinController);
+
+router.get("/test", (req: Request, res: Response) => {
+  res.send("server user api is responding");
+});
+
+export { router as userRouter };
