36112af44f76e72c3d2dd71a575dc2df7ac31ac2
codemesid <siddhanthbajoria@gmail.com>
2020-06-23 16:04:33 +0530

    user routes added

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from "express";
+import { userRouter } from "./user-router";
 
 const router = express.Router();
 
-router.get("/", (req: Request, res: Response) => {
+router.use("/user", userRouter);
+
+router.get("/test", (req: Request, res: Response) => {
   res.send("server api is responding");
 });
 
