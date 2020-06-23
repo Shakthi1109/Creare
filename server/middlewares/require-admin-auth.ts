aa258d62c433fff0e6945dd665af4a327c7efefc
codemesid <siddhanthbajoria@gmail.com>
2020-06-23 16:03:18 +0530

    check specifically for admin status of user

---
+++ server/middlewares/require-admin-auth.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from "express";
+import { NotAuthorizedError } from "../errors/not-authorized-error";
+import { UserRole } from "../util/enum/user-roles";
+
+export const requireAuth = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.currentUser && req.currentUser.role !== UserRole.Admin) {
+    throw new NotAuthorizedError();
+  }
+  next();
+};
