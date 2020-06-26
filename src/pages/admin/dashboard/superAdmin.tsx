85bf26937d4461295e25bd2f73f2d7aa02f8406c
shakthi1109 <mail2shakthivel98@gmail.com>
2020-06-26 20:40:13 +0530

    admin dashboard css added

--- src/pages/admin/dashboard/superAdmin.tsx
+++ src/pages/admin/dashboard/superAdmin.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Sidebar from "../../../components/sidebar"
 
 export default () => {
@@ -5,8 +6,30 @@ export default () => {
         <>
         <Sidebar curr={"superAdmin"} />
         <div className='dashboard'>
-				<h1>Super Admin</h1>
-			</div>
+			<h1>Super Admin</h1>
+		</div>
+        <div className="superAdmin">
+                <form id="superAdmin-form">
+                    <input type="text" placeholder="Enter Text"/>
+                    <button type="submit">Add</button>
+                </form>
+        </div>
+        
         </>
     )
 }
+
+// class superAdmin extends React.Component{
+//     render(){
+//         return(
+//             <>
+//             <Sidebar curr={"superAdmin"} />
+//             <div className='dashboard'>
+//  				<h1>Super Admin</h1>
+//  			</div>
+//             </>
+//         );
+//     }
+// }
+
+// export default superAdmin;
