a2410ef3e1d7840364558d06c31311d2b2e67f79
shakthi1109 <mail2shakthivel98@gmail.com>
2020-06-29 20:25:04 +0530

    changes update

--- src/pages/teacherLanding/index.tsx
+++ src/pages/teacherLanding/index.tsx
@@ -1,1 +1,20 @@
-export default ()=> <div><h1>Teacher Landing Page</h1></div>
+import React from 'react';
+import logo from './logo.png';
+
+function teacherLanding() {
+    return (
+        <body className="teacherBody">
+
+            <div id="logo">
+                <img src={logo} alt="Logo" />
+            </div>
+
+            <div id="teacherHeading">
+                <h1>Teacher Workspace</h1>
+            </div>
+
+        </body>
+    )
+  }
+
+  export default teacherLanding;
