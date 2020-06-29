76ef101e6e273c66a1f8640398d64703d727e9d2
shakthi1109 <mail2shakthivel98@gmail.com>
2020-06-30 02:03:21 +0530

    Teacher Landing Header done

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
-module.exports = {
+const withImages = require("next-images");
+module.exports = withImages({
   webpackDevMiddleware: (config) => {
     config.watchOptions.poll = 300;
     return config;
   },
-};
+});
+
+
+// const withImages = require('next-images')
+// module.exports = withImages({})
+
+// module.exports = {
+//   webpackDevMiddleware: (config) => {
+//     config.watchOptions.poll = 300;
+//     return config;
+//   },
+// };
+
+
+// const withImages = require('next-images')
+// module.exports = withImages({})
+//   webpack(config, options) {
+//     return config
+//   }
+// })
--- src/pages/teacherLanding/index.tsx
+++ src/pages/teacherLanding/index.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
-import logo from './logo.png';
+import logoImg from '../../public/static/logo1.png';
+import {FaBars} from "react-icons/fa";
+import Avatar from 'react-avatar';
 
+const avatarStyle = {
+    
+
+};
 function teacherLanding() {
     return (
-        <body className="teacherBody">
+        <div className="header">
 
-            <div id="logo">
-                <img src={logo} alt="Logo" />
-            </div>
-
-            <div id="teacherHeading">
-                <h1>Teacher Workspace</h1>
-            </div>
-
-        </body>
+            <FaBars id='teacherMenuicon' />
+                
+            <img src={logoImg} alt='logo' id='logoImg'/>
+                
+            <h1 id="teacherHeading">Teacher Workspace</h1>
+            
+            
+            <Avatar name="Foo Bar" color ="gray"size="50" round={true} style={{
+                                                                    float: 'right',
+                                                                    position:'relative',
+                                                                    top: '-45px',
+                                                                    right: '15px',
+                                                                }}/>
+        </div> 
     )
   }
 
