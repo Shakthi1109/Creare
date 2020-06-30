3cb868bab408c2546c7c3068e9114216fb61d82e
shakthi1109 <mail2shakthivel98@gmail.com>
2020-06-30 20:16:50 +0530

    profile icon list view added

--- src/pages/teacherLanding/index.tsx
+++ src/pages/teacherLanding/index.tsx
@@ -1,22 +1,24 @@
 import React from "react"
-// import logoImg from "../../public/static/logo1.png"
+import logoImg from "../../public/static/logo1.png"
 import { FaBars, FaUserAlt } from "react-icons/fa"
 
 const avatarStyle = {}
 function teacherLanding() {
 	return (
 		<div className='header'>
-			<FaBars id='teacherMenuicon' />
+			<FaBars id='teacherMenuicon' color='white' />
 
-			{/* <img src={logoImg} alt='logo' id='logoImg' /> */}
+			<img src={logoImg} alt='logo' id='logoImg' />
 
 			<h1 id='teacherHeading'>Teacher Workspace</h1>
 
 			<span id='floatLeft'>
-				<FaUserAlt />
+				<FaUserAlt color='white'/>
 				<div className='options'>
-					<li>Teacher Profile</li>
-					<li>Class</li>
+					<ul>Profile</ul>
+					<ul>Settings</ul>
+                    <ul>Logout</ul>
+
 				</div>
 			</span>
 		</div>
