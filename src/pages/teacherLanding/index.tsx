5c36f7558223af89e5ee84fc22a0bd0ea03b2227
Maneesh <maneeshvijaykar@gmail.com>
2020-06-30 19:29:59 +0530

    right icon

--- src/pages/teacherLanding/index.tsx
+++ src/pages/teacherLanding/index.tsx
@@ -1,31 +1,26 @@
-import React from 'react';
-import logoImg from '../../public/static/logo1.png';
-import {FaBars} from "react-icons/fa";
-import Avatar from 'react-avatar';
+import React from "react"
+// import logoImg from "../../public/static/logo1.png"
+import { FaBars, FaUserAlt } from "react-icons/fa"
 
-const avatarStyle = {
-    
-
-};
+const avatarStyle = {}
 function teacherLanding() {
-    return (
-        <div className="header">
+	return (
+		<div className='header'>
+			<FaBars id='teacherMenuicon' />
 
-            <FaBars id='teacherMenuicon' />
-                
-            <img src={logoImg} alt='logo' id='logoImg'/>
-                
-            <h1 id="teacherHeading">Teacher Workspace</h1>
-            
-            
-            <Avatar name="Foo Bar" color ="gray"size="50" round={true} style={{
-                                                                    float: 'right',
-                                                                    position:'relative',
-                                                                    top: '-45px',
-                                                                    right: '15px',
-                                                                }}/>
-        </div> 
-    )
-  }
+			{/* <img src={logoImg} alt='logo' id='logoImg' /> */}
 
-  export default teacherLanding;
+			<h1 id='teacherHeading'>Teacher Workspace</h1>
+
+			<span id='floatLeft'>
+				<FaUserAlt />
+				<div className='options'>
+					<li>Teacher Profile</li>
+					<li>Class</li>
+				</div>
+			</span>
+		</div>
+	)
+}
+
+export default teacherLanding
